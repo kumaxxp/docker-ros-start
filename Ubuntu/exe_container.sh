9f311cbdd9a86023d241d82dd5c2860624786186
kumaxxp <kuma_diy@hotmail.com>
2020-05-09 10:21:15 +0900

    Ubuntu上で動く、gazebo+ros2のコンテナを作成

---
+++ Ubuntu/exe_container.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker start my-running-app
+
+docker exec -it my-running-app bash
---
+++ Ubuntu/start_container.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run --privileged -it \
+#           -e "http_proxy=" \
+#           -e "https_proxy=" \
+           --volume=$XSOCK:$XSOCK:rw \
+           --volume=$XAUTH:$XAUTH:rw \
+           --volume=/dev:/dev:rw \
+           --shm-size=1gb \
+           --env="XAUTHORITY=${XAUTH}" \
+           --env="DISPLAY=${DISPLAY}" \
+           --env=TERM=xterm-256color \
+           --env=QT_X11_NO_MITSHM=1 \
+           --net=host \
+	   -v="/tmp/.gazebo/:/root/.gazebo/" --name my-running-app my-gazebo-app \
+           bash
+
+
+# https://hub.docker.com/_/gazebo
+
