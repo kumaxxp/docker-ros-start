374d855e0086bf1be8844c93b7ce2de20cffb1bf
kumaxxp <kuma_diy@hotmail.com>
2020-05-10 17:47:43 +0900

    ROS向けのDockerfileを作ってみた

---
+++ Ubuntu/gazebo_ros/build.sh
@@ -0,0 +1,1 @@
+docker build -t my-ros-gazebo-app .
---
+++ Ubuntu/gazebo_ros/exe_container.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker start my-ros-running-app
+
+docker exec -it my-ros-running-app bash
