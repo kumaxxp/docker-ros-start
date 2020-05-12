6ee10ad8abbe94de903b16e0c6e891eda0d78bb0
kumaxxp <kuma_diy@hotmail.com>
2020-05-12 13:22:20 +0900

    cuda_gui_dockerを作る。GPU付きPCでGUIが動くコンテナ

---
+++ Ubuntu/cuda_gui_docker/start.sh
@@ -0,0 +1,25 @@
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+docker run --gpus all --privileged --rm -it \
+           --volume=$XSOCK:$XSOCK:rw \
+           --volume=$XAUTH:$XAUTH:rw \
+           --volume=$HOME:$HOME \
+           --shm-size=1gb \
+           --env="XAUTHORITY=${XAUTH}" \
+           --env="DISPLAY=${DISPLAY}" \
+           --env=TERM=xterm-256color \
+           --env=QT_X11_NO_MITSHM=1 \
+           --net=host \
+           -u "cuda10"  \
+           ros2_gpu \
+           bash
+
+# GUIが正常に動作することを確認した
+# xeyes を実行するために、この環境にインストール
+# sudo apt update
+# sudo apt install x11-apps
+# xeyes
+# で、画面に表示されることを確認した。
